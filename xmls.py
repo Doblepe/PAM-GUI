e4ee0c66e92313c4307e83f6587a178928d5e808
vicdoblepe <vicdoblepe@pop-os.localdomain>
2023-01-07 11:14:13 +0100

    xmlx writer-full

--- xmls.py
+++ xmls.py
@@ -31,7 +31,27 @@ def exportXLSX():
         worksheet.write(40, 8, 'Recuperadas', header_cell)
         # Creo la cuenta de coordinaciones
         worksheet.write(60, 0, 'COORDINACIONES', header_cell)
-
+        # Sumatorio de los públicos
+        worksheet.write(37,6,'=SUMA(G2:G37)', header_cell )
+        worksheet.write(37,5,'Horas/Mes', header_cell )
+        worksheet.write(38, 6,'=G38*40', header_cell)
+        worksheet.write(38, 5, 'TOTAL', header_cell )
+        # Sumatorio de los privados
+        worksheet.write(52,6,'=SUMA(G42:G52)', header_cell )
+        worksheet.write(52,5,'Horas/Mes Privadas', header_cell )
+        worksheet.write(53, 6,'=G53*45', header_cell)
+        worksheet.write(53, 5, 'TOTAL', header_cell )
+        #Sumatorio de coordinaciones
+        worksheet.write(70, 1, '=SUMA(B60:B69)', header_cell)
+        worksheet.write(70, 0, 'TOTAL DE COORDINACIONES', header_cell)
+        #Final result
+        worksheet.write(57,5,'Total generado', header_cell )
+        worksheet.write(57,6,'=G39+G53+B71', header_cell )
+        worksheet.write(58,5,'50%' )
+        worksheet.write(58,6,'=G58/2')
+        worksheet.write(59,5,'-15%' )
+        worksheet.write(60,6,'=G58-G59', header_cell  )
+        worksheet.write(60,5,'TOTAL EUR')
    # Start from the first cell. Rows and columns are zero indexed.
         row = 1
         col = 0
@@ -50,6 +70,7 @@ def exportXLSX():
         for item in Privados_list:
             worksheet.write(row, col, item[0])
             row += 1
+
         try:
             row = 0
             col = 1
@@ -65,6 +86,7 @@ def exportXLSX():
                     if month == '01' and nombre_peke == nombre_cita:
                         worksheet.write(row, col, cita[2])
                         col +=1
+                worksheet.write(row,6, col -1)
         except Exception as e:
             print(e)    
         try:
@@ -80,8 +102,9 @@ def exportXLSX():
                     nombre_cita = cita[1]
                     month = cita[2][5] + cita[2][6] 
                     if month == '01' and nombre_peke == nombre_cita:
-                        worksheet.write(row, col, cita[2])
+                        worksheet.write(row, col, (cita[2]))
                         col +=1
+                worksheet.write(row,6, col -1)
         except Exception as e:
             print(e)
         try:
@@ -94,6 +117,7 @@ def exportXLSX():
                 if month == '01':
                     row += 1 
                     worksheet.write(row, col, result[1])
+                    worksheet.write(row, col+1, 36.28)
         except Exception as e:
             print(e)
 
